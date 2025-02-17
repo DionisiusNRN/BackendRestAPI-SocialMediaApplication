40f895628c8866dfa1c4e4f4b053079887d94420
DionisiusNRN <dnur3no@gmail.com>
2025-02-17 07:40:29 +0700

    Menambahkan method pada MessageController serta fillable dan softdeletes pada Model Message

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Message;
+use Illuminate\Support\Facades\Validator;
 
 class MessagesController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'sender_id' => 'required',
+            'receiver_id'=> 'required',
+            'message_content'=> 'required|string|max:255',
+        ]);
+
+        // Jika gagal, maka
+        if ($validator->fails()) {
+            return response()->json([
+                'success'=> false,
+                'message'=> $validator->errors(),
+            ], 400) ;
+        }
+
+        // Lanjut jika validasi OK
+        $message = Message::create([
+            'sender_id' => $request->sender_id,
+            'receiver_id'=> $request->receiver_id,
+            'message_content'=> $request->message_content,
+        ]);
+
+        return response()->json([
+            'success'=> true,
+            'message'=> 'Berhasil mengirim pesan',
+            'data'=> $message,
+        ]);
+    }
+
+    public function show($id) {
+        $message = Message::find($id);
+
+        return response()->json([
+            'success'=> true,
+            'message'=> 'Berhasil mengambil pesan',
+            'data'=> $message
+        ]);
+    }
+
+    public function destroy($id) {
+        Message::destroy($id);
+
+        return response()->json([
+            'success'=> true,
+            'message'=> 'Pesan berhasil dihapus'
+        ]);
+    }
 }
